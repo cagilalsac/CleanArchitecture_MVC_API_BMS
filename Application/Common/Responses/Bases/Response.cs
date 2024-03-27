4006d6db6b34e6761d264b9b7e77e4f3dc99bf7d
Çağıl Alsaç <cagilalsac@outlook.com>
2024-03-27 21:24:03 +0300

    vFinal

--- Application/Common/Responses/Bases/Response.cs
+++ Application/Common/Responses/Bases/Response.cs
@@ -2,13 +2,13 @@
 
 namespace Application.Common.Responses.Bases
 {
-    public record Response : IRecord
+    public abstract record Response : IRecord
     {
         public int Id { get; set; }
         public bool IsSuccessful { get; }
         public string Message { get; }
 
-        public Response(bool isSuccessful, string message, int id)
+        protected Response(bool isSuccessful, string message, int id)
         {
             IsSuccessful = isSuccessful;
             Message = message;
